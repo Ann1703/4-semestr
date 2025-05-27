b636b7d5b01e890b8b729082448acfbc31a6faa5
Ann1703 <79947765+Ann1703@users.noreply.github.com>
2025-05-27 16:10:18 +0300

    Add files via upload

--- zadanie_4.cpp
+++ zadanie_4.cpp
@@ -6,31 +6,44 @@
 
 using namespace std;
 
-struct point {
+class Point {
+private:
     int x, y;
     string s;
+
+public:
+   Point() : x(0), y(0), s("") {}
+
+    // Методы доступа
+    int getX() const { return x; }
+    int getY() const { return y; }
+    string getS() const { return s; }
+
+    void setX(int x) { this->x = x; }
+    void setY(int y) { this->y = y; }
+    void setS(const string& s) { this->s = s; }
+
+    // Оператор ввода
+    friend istream& operator>>(istream& is, Point& p) {
+        is >> p.x >> p.y >> p.s;
+        return is;
+    }
+
+    // Оператор вывода
+    friend ostream& operator<<(ostream& os, const Point& p) {
+        os << p.x << " " << p.y << " " << p.s;
+        return os;
+    }
+
+    // Оператор сложения
+    Point operator+(const Point& other) const {
+        Point result;
+        result.x = this->x + other.x;
+        result.y = this->y + other.y;
+        result.s = this->s + other.s;
+        return result;
+    }
 };
-
-// Оператор ввода для point
-istream& operator>>(istream& is, point& p) {
-    is >> p.x >> p.y >> p.s;
-    return is;
-}
-
-// Оператор вывода для point
-ostream& operator<<(ostream& os, const point& p) {
-    os << p.x << " " << p.y << " " << p.s;
-    return os;
-}
-
-// Оператор сложения для point
-point operator+(const point& a, const point& b) {
-    point result;
-    result.x = a.x + b.x;
-    result.y = a.y + b.y;
-    result.s = a.s + b.s;
-    return result;
-}
 
 int main() {
     setlocale(LC_ALL, "Russian");
@@ -40,8 +53,8 @@ int main() {
     const string name2 = "name2.txt";
 
     // Чтение данных из файлов в векторы V1 и V2
-    vector<point> V1, V2;
-    point p;
+    vector<Point> V1, V2;
+    Point p;
 
     // Открытие первого файла
     ifstream file1(name1);
@@ -74,7 +87,7 @@ int main() {
     }
 
     // Сложение элементов V1 и V2 с использованием transform
-    transform(V1.begin(), V1.end(), V2.begin(), V1.begin(), plus<point>());
+    transform(V1.begin(), V1.end(), V2.begin(), V1.begin(), plus<Point>());
 
     // Запись результата обратно в name1
     ofstream outFile(name1);
